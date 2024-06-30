44cb845ce2139b9579f81e71f96af24c8113b3fb
Ibrahim Al-Tamimi (ARK) <ibm.iloom@gmail.com>
2024-06-30 13:22:56 +0300

    feat: add default spring starter for Kotlin exposed #13

--- radix-development/radix-development-exposed-table/src/main/java/io/arkitik/radix/develop/exposed/table/RadixTable.kt
+++ radix-development/radix-development-exposed-table/src/main/java/io/arkitik/radix/develop/exposed/table/RadixTable.kt
@@ -8,7 +8,7 @@ import org.jetbrains.exposed.sql.javatime.datetime
  * Created By Ibrahim Al-Tamimi 
  * Created At 12:05 PM, 30/06/2024
  */
-abstract class RadixTable<ID : Comparable<ID>> : Table() {
+abstract class RadixTable<ID : Comparable<ID>>(name: String = "") : Table(name) {
     abstract val uuid: Column<ID>
     val creationDate = datetime(name = "creation_date")
 
