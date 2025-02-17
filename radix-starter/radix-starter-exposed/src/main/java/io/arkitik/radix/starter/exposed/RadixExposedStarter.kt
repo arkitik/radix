c94d36460d6a00331e62a13e8f9deae1e3e09d41
Ibrahim Al-Tamimi (ARK) <ibm.iloom@gmail.com>
2025-02-17 14:47:28 +0300

    fix: remove RadixSpringTransactionManager and create a separated database instance to be used within the application #20

--- radix-starter/radix-starter-exposed/src/main/java/io/arkitik/radix/starter/exposed/RadixExposedStarter.kt
+++ radix-starter/radix-starter-exposed/src/main/java/io/arkitik/radix/starter/exposed/RadixExposedStarter.kt
@@ -1,18 +1,14 @@
 package io.arkitik.radix.starter.exposed
 
-import io.arkitik.radix.starter.exposed.function.RadixSpringTransactionManager
 import org.jetbrains.exposed.spring.autoconfigure.ExposedAutoConfiguration
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.DatabaseConfig
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import javax.sql.DataSource
 
@@ -22,13 +18,7 @@ import javax.sql.DataSource
  */
 @Configuration
 @AutoConfiguration(after = [DataSourceAutoConfiguration::class])
-@ImportAutoConfiguration(
-    value = [ExposedAutoConfiguration::class],
-    exclude = [
-        DataSourceTransactionManagerAutoConfiguration::class,
-        ExposedAutoConfiguration::class
-    ]
-)
+@ImportAutoConfiguration(ExposedAutoConfiguration::class)
 @EnableTransactionManagement
 class RadixExposedStarter {
     @Bean
@@ -47,16 +37,5 @@ class RadixExposedStarter {
             datasource = datasource,
             databaseConfig = databaseConfig
         )
-    }
-
-    @Bean
-    @Primary
-    fun springTransactionManager(
-        database: Database,
-        databaseConfig: DatabaseConfig,
-        @Value("\${spring.exposed.show-sql:false}")
-        showSql: Boolean = false,
-    ): RadixSpringTransactionManager {
-        return RadixSpringTransactionManager(database, databaseConfig, showSql)
     }
 }
